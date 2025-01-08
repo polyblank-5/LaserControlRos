91b923badd253e089ae493590329825cbb646271
Jan Achermann <achermann@campus.tu-berlin.de>
2025-01-08 16:14:15 +0100

    change name + publishers

--- laser_control_pkg/laser_position_publisher.py
+++ laser_control_pkg/laser_position_publisher.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64
+from std_msgs.msg import Float32MultiArray
+from laser_control_pkg.laser_measured_publisher import LaserMeasuredPublisher
 '''
 This node gets the positions from the weed list
 It checks if one of the weed is in the laser area
@@ -8,31 +9,33 @@ if so its position is getting published
 checks laser status to see if weed is killed 
 
 '''
-class LaserDesiredPublisher(Node):
+class LaserPositionPublisher(Node):
     def __init__(self):
-        super().__init__('laser_desired_publisher')
-        self.publisher_ = self.create_publisher(Float64, 'desired_laser', 10)
+        super().__init__('laser_position_publisher')
+        self.publisher_ = self.create_publisher(Float32MultiArray, 'position_laser', 10)
         self.subscription = self.create_subscription(
-            Float64,
-            'control_input',
+            Float32MultiArray,
+            'weed_position_laser_area',
             self.control_callback,
             10)
         self.subscription  # Prevent unused variable warning
         self.timer = self.create_timer(1.0, self.publish_desired_value)
+        self.x_weed:float = -1.0
+        self.y_weed:float = -1.0
 
     def publish_desired_value(self):
-        msg = Float64()
+        msg = Float32MultiArray()
         msg.data = 10.0  # Replace with your logic
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing Desired Value: {msg.data}')
 
-    def control_callback(self, msg):
+    def control_callback(self, msg:Float32MultiArray):
         self.get_logger().info(f'Received Control Input: {msg.data}')
         # Process received data
 
 def main(args=None):
     rclpy.init(args=args)
-    node = LaserDesiredPublisher()
+    node = LaserPositionPublisher()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
